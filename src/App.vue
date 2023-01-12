d8bdb19bad0c604aaa72a6a58b08fef26f343ece
serendipity527 <79218767+serendipity527@users.noreply.github.com>
2023-01-12 19:44:25 +0800

    vue项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Demo from './components/Demo.vue';
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <Demo></Demo>
 </template>
 
 <style scoped>
